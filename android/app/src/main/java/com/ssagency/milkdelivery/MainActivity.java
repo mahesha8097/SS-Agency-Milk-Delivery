825d1a6220faf6cbe1fe3a9eed1ce0c1937e2d21
Mahesha <mahesha8097@gmail.com>
2026-08-24 12:02:21 +0530

    Configure static export and Capacitor Android platform assets

---
+++ android/app/src/main/java/com/ssagency/milkdelivery/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ssagency.milkdelivery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+
   // Header optimization for PWA
   async headers() {
     return [
